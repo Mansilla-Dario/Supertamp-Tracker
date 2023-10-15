1913323531aafa3bb9597b6273a499b3af770640
Mansilla-Dario <mansillagerman@gmail.com>
2023-10-15 10:56:21 -0500

    First Configurations of the Project

---
+++ components/views/DashboardGeneralView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DashboardGeneralView() {
+  return (
+    <div>dashboardGeneralView</div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, '.');
+
+    return config;
+  },
+};
